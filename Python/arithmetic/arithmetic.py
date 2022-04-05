f43d3359b1cc5ce7e84552be8dd08bffa0d6315a
ZakharovVladyslav <vladislavozakharov@gmail.com>
2022-04-05 16:34:40 +0300

    Added choice

--- Python/arithmetic/arithmetic.py
+++ Python/arithmetic/arithmetic.py
@@ -27,30 +27,49 @@ def arithmetic(num1, num2, action):
     return num1
 
 # main section 
-    
-num1 = int(input("Enter num1: "))
-num2 = int(input("Enter num2: "))
-action = str(input("Enter action: "))
-count = 1
 
-file = open("results.txt", "a")
+print("What do you want to choose? (choose from the list below)")
+print("> Calculator - calc\n>Check all results - file")
+choice = str(input("Choice:"))
 
-result = arithmetic(num1, num2, action)
-print("Result: ", result)
-file.write("\n")
-file.write(f"Result {count}: {result}\n")    
-
-answer = str(input("Want to continue? (yes / no): "))
-count += 1
-
-while answer != "no":
+if choice == "calc":
     num1 = int(input("Enter num1: "))
     num2 = int(input("Enter num2: "))
     action = str(input("Enter action: "))
-        
+    count = 1
+
+    file = open("results.txt", "a")
+
     result = arithmetic(num1, num2, action)
-    print("Result: ", result)  
-    file.write(f"Result {count}: {result}\n")
-        
+    print("Result: ", result)
+    file.write("\n")
+    file.write(f"Result {count}: {result}\n")    
+
     answer = str(input("Want to continue? (yes / no): "))
     count += 1
+
+    if answer == "yes":
+        while answer != "no":
+            num1 = int(input("Enter num1: "))
+            num2 = int(input("Enter num2: "))
+            action = str(input("Enter action: "))
+                
+            result = arithmetic(num1, num2, action)
+            print("Result: ", result)  
+            file.write(f"Result {count}: {result}\n")
+                
+            answer = str(input("Want to continue? (yes / no): "))
+            count += 1
+            
+    file.close()
+            
+elif choice == "file":
+    file = open("results.txt", "r")
+    
+    print(file.read())
+    
+    file.close()
+    
+else:
+    print("Wrong variant")
+    os._exit()
